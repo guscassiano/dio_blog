1096a33260e647ea0b93f9e875924eddca3aec22
Gustavo Cassiano <gucpinto26@gmail.com>
2025-12-26 00:34:48 -0300

    :bug:fix: install psycopg2 and asyncpg to ecute with postgresql database

--- src/database.py
+++ src/database.py
@@ -9,4 +9,7 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./blog.db")
 database = databases.Database(DATABASE_URL)
 metadata = sa.MetaData()
 
-engine = sa.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+if os.getenv("RENDER"):
+    engine = sa.create_engine(DATABASE_URL)
+else:
+    engine = sa.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
